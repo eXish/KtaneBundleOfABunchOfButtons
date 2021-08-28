62c0382290fe69ad7fefaa55a2ec9f338f3eec90
JakkOfKlubs <qwuest123@gmail.com>
2021-08-28 12:28:21 -0400

    Initial commit.

---
+++ Assets/Modules/OrangeButtonModule/OrangeButtonScript.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+
+public class OrangeButtonScript : MonoBehaviour
+{
+    [SerializeField]
+    private KMBombModule Module;
+    [SerializeField]
+    private KMBombInfo BombInfo;
+    [SerializeField]
+    private KMAudio Audio;
+    [SerializeField]
+    private KMRuleSeedable RuleSeedable;
+    [SerializeField]
+    private KMSelectable OrangeButtonSelectable;
+    [SerializeField]
+    private GameObject OrangeButtonCap;
+    [SerializeField]
+    private MeshRenderer[] LedLights;
+    [SerializeField]
+    private Material[] LedMats;
+    [SerializeField]
+    private TextMesh _orangeButtonText;
+    [SerializeField]
+    private TextMesh[] _mainText;
+    [SerializeField]
+    private Color[] _textColors;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId, _lastTimerSeconds, _lightIndex, _tapIx, _buttonNum;
+    private int[] _lightCycle = { 0, 1, 2, 3 }, _screenTextIxs = new int[3], _snDigits = new int[6], _snPairs = new int[6], _solutions = new int[3], _pairPos = { 0, 1, 2, 3, 4, 5 };
+    private List<int> letterTable = new List<int>();
+    private string LETTERS = "ABCDEFGHI";
+    private string[] POSITIONS = { "first", "second", "third", "fourth", "fifth", "sixth" };
+    private bool _moduleSolved, _flashingStrike, _buttonHeld, _checkTap, _isStriking;
+    private Coroutine _animateButton;
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+
+        // START RULE SEED
+        var rnd = RuleSeedable.GetRNG();
+        Debug.LogFormat("[The Orange Button #{0}] Using rule seed {1}", _moduleId, rnd.Seed);
+        rnd.Next(0, 2);
+        rnd.Next(0, 2);
+        for (int ix = 0; ix < 9; ix++)
+        {
+            int[] letIx = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            rnd.ShuffleFisherYates(letIx);
+            for (int jx = 0; jx < 9; jx++)
+            {
+                letterTable.Add(letIx[jx]);
+            }
+        }
+        rnd.ShuffleFisherYates(_pairPos);
+        // END RULE SEED
+
+        OrangeButtonSelectable.OnInteract += delegate ()
+        {
+            OrangeButtonPress();
+            return false;
+        };
+        OrangeButtonSelectable.OnInteractEnded += delegate ()
+        {
+            OrangeButtonRelease();
+        };
+
+        for (int i = 0; i < _screenTextIxs.Length; i++)
+            _screenTextIxs[i] = Rnd.Range(0, 9);
+
+        string SerialNumber = BombInfo.GetSerialNumber();
+        for (int i = 0; i < _snDigits.Length; i++)
+            _snDigits[i] = (SerialNumber[i] >= '0' && SerialNumber[i] <= '9' ? SerialNumber[i] - '0' : SerialNumber[i] - 'A' + 10) % 9;
+
+        _buttonNum = Rnd.Range(0, 9);
+        _orangeButtonText.text = _buttonNum.ToString();
+        SetText();
+
+        _lightCycle.Shuffle();
+        SetLights();
+
+        FindSolution();
+    }
+
+    private void OrangeButtonPress()
+    {
+        _animateButton = StartCoroutine(AnimateButton(0f, -0.05f));
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonPress, transform);
+        if (!_moduleSolved)
+        {
+            _checkTap = false;
+            _buttonHeld = true;
+        }
+    }
+    private void OrangeButtonRelease()
+    {
+        _animateButton = StartCoroutine(AnimateButton(-0.05f, 0f));
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonRelease, transform);
+        if (!_moduleSolved && !_isStriking)
+        {
+            _buttonHeld = false;
+            if (_checkTap)
+                CheckAnswer();
+            else
+            {
+                SetLights();
+                ButtonTap(_lastTimerSeconds);
+            }
+        }
+    }
+
+    private IEnumerator AnimateButton(float a, float b)
+    {
+        var duration = 0.1f;
+        var elapsed = 0f;
+        while (elapsed < duration)
+        {
+            OrangeButtonCap.transform.localPosition = new Vector3(0f, Easing.InOutQuad(elapsed, a, b, duration), 0f);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+    }
+
+    private void SetLights()
+    {
+        _lightIndex++;
+        for (int i = 0; i < _lightCycle.Length; i++)
+        {
+            if (i != _lightCycle[(_lightIndex + 1) % 4])
+                LedLights[i].material = LedMats[0];
+            else
+                LedLights[i].material = LedMats[1];
+        }
+
+    }
+
+    private void Update()
+    {
+        var seconds = (int)BombInfo.GetTime() % 3;
+        if (seconds != _lastTimerSeconds)
+        {
+            _lastTimerSeconds = seconds;
+            if (!_flashingStrike)
+                SetTextColors(seconds);
+            if (_buttonHeld)
+                _checkTap = true;
+        }
+    }
+
+    private void SetText()
+    {
+        for (int i = 0; i < _mainText.Length; i++)
+            _mainText[i].text = LETTERS.Substring(_screenTextIxs[i], 1);
+    }
+    private void SetTextColors(int sec)
+    {
+        if (!_moduleSolved && !_isStriking)
+        {
+            for (int i = 0; i < _mainText.Length; i++)
+            {
+                if (i == sec)
+                    _mainText[i].color = _textColors[3];
+                else
+                    _mainText[i].color = _textColors[2];
+            }
+        }
+    }
+
+    private void ButtonTap(int ix)
+    {
+        _screenTextIxs[ix] = (_screenTextIxs[ix] + _lightCycle[_lightIndex % 4] + 1) % 9;
+        SetText();
+    }
+
+    private void FindSolution()
+    {
+        for (int i = 0; i < _snPairs.Length; i++)
+            _snPairs[i] = _snDigits[_pairPos[i]];
+        for (int i = 0; i < _solutions.Length; i++)
+            _solutions[i] = letterTable[((_snPairs[i * 2] + _buttonNum) % 9) + (_snPairs[i * 2 + 1] * 9)];
+        Debug.LogFormat("[The Orange Button #{0}] The solution is {1}, {2}, {3}.", _moduleId, LETTERS[_solutions[0]], LETTERS[_solutions[1]], LETTERS[_solutions[2]]);
+    }
+
+    private void CheckAnswer()
+    {
+        bool isSolve = true;
+        for (int i = 0; i < _screenTextIxs.Length; i++)
+            if (_solutions[i] != _screenTextIxs[i])
+                isSolve = false;
+        if (isSolve)
+        {
+            Module.HandlePass();
+            _moduleSolved = true;
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+            for (int i = 0; i < _screenTextIxs.Length; i++)
+                _mainText[i].color = _textColors[1];
+            for (int i = 0; i < LedLights.Length; i++)
+                LedLights[i].material = LedMats[1];
+            Debug.LogFormat("[The Orange Button #{0}] Submitted {1}, {2}, {3}. Module solved.", _moduleId, LETTERS[_screenTextIxs[0]], LETTERS[_screenTextIxs[1]], LETTERS[_screenTextIxs[2]]);
+        }
+        else
+            StartCoroutine(Strike());
+    }
+
+    private IEnumerator Strike()
+    {
+        Debug.LogFormat("[The Orange Button #{0}] Submitted {1}, {2}, {3}. Strike.", _moduleId, LETTERS[_screenTextIxs[0]], LETTERS[_screenTextIxs[1]], LETTERS[_screenTextIxs[2]]);
+        Module.HandleStrike();
+        _isStriking = true;
+        for (int i = 0; i < _screenTextIxs.Length; i++)
+        {
+            if (_screenTextIxs[i] != _solutions[i])
+                _mainText[i].color = _textColors[0];
+            else
+                _mainText[i].color = _textColors[1];
+        }
+        yield return new WaitForSeconds(1.2f);
+        _isStriking = false;
+    }
+}
---
+++ Assets/Modules/RedButtonModule/RedButtonScript.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+public class RedButtonScript : MonoBehaviour
+{
+
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+    public KMSelectable RedButtonSelectable;
+    public GameObject RedButtonCap;
+    public TextMesh _buttonText;
+    public TextMesh[] _topText, _bottomText, _lightsText;
+    public Color[] _textColors;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId, _buttonSymbol, _lastTimerSeconds;
+    private int[] _pairParts = new int[6];
+    private bool _moduleSolved, _buttonHeld, _correctAnswer, _checkTap;
+    private bool[] _topLogic = new bool[10], _bottomLogic = new bool[10], _heldLogic = new bool[10], _solutionLogic = new bool[10];
+
+    private Coroutine _animateButton;
+    private string _logicGates = "∧∨⊻|↓↔→←", topTextDisplay, bottomTextDisplay, solution = "";
+    private string[] _logicGatesWords = { "AND", "OR", "XOR", "NAND", "NOR", "XNOR", "LEFT IMPLIES RIGHT", "RIGHT IMPLIES LEFT" };
+
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+
+        RedButtonSelectable.OnInteract += delegate ()
+        {
+            RedButtonPress();
+            return false;
+        };
+        RedButtonSelectable.OnInteractEnded += delegate ()
+        {
+            RedButtonRelease();
+        };
+        Generate();
+    }
+
+
+    private void RedButtonPress()
+    {
+        _animateButton = StartCoroutine(AnimateButton(0f, -0.05f));
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonPress, transform);
+        if (!_moduleSolved)
+        {
+            _checkTap = false;
+            _buttonHeld = true;
+        }
+    }
+
+    private void RedButtonRelease()
+    {
+        _animateButton = StartCoroutine(AnimateButton(-0.05f, 0f));
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonRelease, transform);
+        if (!_moduleSolved)
+        {
+            _buttonHeld = false;
+            if (!_checkTap)
+                CheckAnswer();
+        }
+    }
+
+    private IEnumerator AnimateButton(float a, float b)
+    {
+        var duration = 0.1f;
+        var elapsed = 0f;
+        while (elapsed < duration)
+        {
+            RedButtonCap.transform.localPosition = new Vector3(0f, Easing.InOutQuad(elapsed, a, b, duration), 0f);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+    }
+    private void Generate()
+    {
+        _buttonSymbol = Rnd.Range(0, 8);
+        _buttonText.text = _logicGates.Substring(_buttonSymbol, 1);
+        Debug.LogFormat("[The Red Button #{0}] The logic gate written on the button is {1}", _moduleId, _logicGatesWords[_buttonSymbol]);
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            int rndTop = Rnd.Range(0, 2);
+            int rndBottom = Rnd.Range(0, 2);
+            int rndColor = Rnd.Range(0, 2);
+
+            _topText[i].text = rndTop == 0 ? "0" : "1";
+            _topLogic[i] = rndTop == 0 ? false : true;
+            _topText[i].color = _textColors[rndColor];
+            topTextDisplay += rndTop == 0 ? "0" : "1";
+
+            _bottomText[i].text = rndBottom == 0 ? "0" : "1";
+            _bottomLogic[i] = rndBottom == 0 ? false : true;
+            _bottomText[i].color = _textColors[rndColor];
+            bottomTextDisplay += rndBottom == 0 ? "0" : "1";
+
+            _lightsText[i].color = _textColors[2];
+
+            _heldLogic[i] = rndColor == 0 ? true : false;
+        }
+        Debug.LogFormat("[The Red Button #{0}] The top display is {1}", _moduleId, topTextDisplay);
+        Debug.LogFormat("[The Red Button #{0}] The bottom display is {1}", _moduleId, bottomTextDisplay);
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (_buttonSymbol == 0) // AND
+                if (_topLogic[i] && _bottomLogic[i])
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 1) // OR
+                if (_topLogic[i] || _bottomLogic[i])
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 2) // XOR
+                if (_topLogic[i] != _bottomLogic[i])
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 3) // NAND
+                if (!(_topLogic[i] && _bottomLogic[i]))
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 4) // NOR
+                if (!(_topLogic[i] || _bottomLogic[i]))
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 5) // XNOR
+                if (_topLogic[i] == _bottomLogic[i])
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 6) // LEFT IMPLIES RIGHT or TOP IMPLIES BOTTOM
+                if (!(_topLogic[i] && !_bottomLogic[i]))
+                    _solutionLogic[i] = true;
+            if (_buttonSymbol == 7) // RIGHT IMPLIES LEFT or BOTTOM IMPLIES TOP
+                if (!(!_topLogic[i] && _bottomLogic[i]))
+                    _solutionLogic[i] = true;
+            if (_solutionLogic[i])
+                solution += "G";
+            if (!_solutionLogic[i])
+                solution += "R";
+        }
+        Debug.LogFormat("[The Red Button #{0}] The correct answer is: {1}", _moduleId, solution);
+    }
+
+    private void Update()
+    {
+        var seconds = (int)BombInfo.GetTime() % 10;
+        if (seconds != _lastTimerSeconds)
+        {
+            _lastTimerSeconds = seconds;
+            SetLights(seconds);
+            if (_buttonHeld)
+            {
+                ToggleLogic(seconds);
+                _checkTap = true;
+            }
+        }
+    }
+
+    private void ToggleLogic(int i)
+    {
+        if (_heldLogic[i])
+        {
+            _heldLogic[i] = false;
+            _topText[i].color = _textColors[1];
+            _bottomText[i].color = _textColors[1];
+        }
+        else
+        {
+            _heldLogic[i] = true;
+            _topText[i].color = _textColors[0];
+            _bottomText[i].color = _textColors[0];
+        }
+    }
+
+    private void SetLights(int sec)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (i != sec)
+                _lightsText[i].color = _textColors[2];
+            if (i == sec)
+                _lightsText[i].color = _textColors[3];
+        }
+    }
+
+    private void CheckAnswer()
+    {
+        string submission = "";
+        _correctAnswer = true;
+        for (int i = 0; i < 10; i++)
+        {
+            if (_heldLogic[i] != _solutionLogic[i])
+                _correctAnswer = false;
+            if (_heldLogic[i])
+                submission += "G";
+            if (!_heldLogic[i])
+                submission += "R";
+        }
+        if (_correctAnswer)
+        {
+            Module.HandlePass();
+            _moduleSolved = true;
+            Debug.LogFormat("[The Red Button #{0}] You submitted {1}. Module solved.", _moduleId, submission);
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+        }
+        else
+        {
+            Module.HandleStrike();
+            Debug.LogFormat("[The Red Button #{0}] You submitted {1}, but the correct answer is {2}. Strike.", _moduleId, submission, solution);
+        }
+    }
+#pragma warning disable 0414
+    private readonly string TwitchHelpMessage = "!{0} hold 1 5 [Hold on 1, release on 5] | !{0} tap";
+#pragma warning restore 0414
+    private IEnumerator ProcessTwitchCommand(string command)
+    {
+        if (_moduleSolved)
+            yield break;
+
+        string[] split = command.ToLowerInvariant().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (split.Length == 1 && split[0] == "tap")
+        {
+            yield return null;
+            var current = (int)BombInfo.GetTime() % 10;
+            while ((int)BombInfo.GetTime() % 10 == current)
+                yield return null;
+            yield return new[] { RedButtonSelectable };
+            yield break;
+        }
+
+        if (split.Length == 3 && split[0] == "hold")
+        {
+            int holdTime, releaseTime;
+            if (!int.TryParse(split[1], out holdTime) || holdTime < 0 || holdTime > 9)
+                yield break;
+            if (!int.TryParse(split[2], out releaseTime) || releaseTime < 0 || releaseTime > 9)
+                yield break;
+            yield return null;
+            while ((int)BombInfo.GetTime() % 10 != holdTime)
+                yield return "trycancel";
+            RedButtonSelectable.OnInteract();
+            while ((int)BombInfo.GetTime() % 10 != releaseTime)
+                yield return null;
+            RedButtonSelectable.OnInteractEnded();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
