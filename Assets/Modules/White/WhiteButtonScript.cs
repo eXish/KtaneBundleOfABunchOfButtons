aee485c9eb9bf973c2508572930799de40c45ee7
Timwi <github@timwi.de>
2021-09-06 02:49:32 +0200

    White Button TP

--- Assets/Modules/White/WhiteButtonScript.cs
+++ Assets/Modules/White/WhiteButtonScript.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
-using Rnd = UnityEngine.Random;
+using System.Text.RegularExpressions;
 using KModkit;
+using UnityEngine;
+
+using Rnd = UnityEngine.Random;
 
 public class WhiteButtonScript : MonoBehaviour
 {
@@ -23,16 +26,17 @@ public class WhiteButtonScript : MonoBehaviour
     private bool _moduleSolved, _colorblindMode;
 
     private bool _buttonHeld;
-    private bool _checkTap;
+    private bool _longPress;
     private int _lastTimerSeconds;
-    private int[] _blobColors = new int[5];
-    private int[] _targetBlobColors = new int[5];
+    private readonly int[] _blobColors = new int[5];
+    private readonly int[] _targetBlobColors = new int[5];
     private int _currentColor;
     private int _currentBlob;
     private bool _isAdding, _isStriking;
     private readonly string[] COLORNAMES = { "Iridium", "East Bay", "Cerulean", "Laurel", "Celadon", "Seaport", "Apple", "Emerald", "Pelorous", "Lotus", "Plum", "Orchid", "Sycamore", "Battleship", "Cove", "Atlantis", "Pistachio", "Neptune", "Mahogany", "Mulberry", "Amethyst", "Sienna", "Puce", "Viola", "Turmeric", "Pine", "Silver" };
     private readonly string[] CBNAMES = { "Ird", "Est", "Cer", "Lau", "Cdn", "Spt", "Apl", "Emd", "Pls", "Lot", "Plm", "Orc", "Syc", "Btl", "Cov", "Atl", "Pch", "Npt", "Mhg", "Mlb", "Amt", "Snn", "Pce", "Vio", "Trm", "Pin", "Slv" };
     private readonly string[] POSITIONS = { "first", "second", "third", "fourth", "fifth" };
+
     private void Start()
     {
         _moduleId = _moduleIdCounter++;
@@ -60,9 +64,9 @@ public class WhiteButtonScript : MonoBehaviour
         {
             _blobColors[i] = Rnd.Range(0, 26);
             var color = new Color(
-                (60 + 70 * ((float)(_blobColors[i] / 9))) / 255,
-                (60 + 70 * ((float)(_blobColors[i] % 9) / 3)) / 255,
-                (60 + 70 * ((float)(_blobColors[i] % 3))) / 255);
+                (60 + 70 * ((float) (_blobColors[i] / 9))) / 255,
+                (60 + 70 * ((float) (_blobColors[i] % 9) / 3)) / 255,
+                (60 + 70 * ((float) (_blobColors[i] % 3))) / 255);
             ColorBlobs[i].material.color = color;
             Debug.LogFormat("[The White Button #{0}] The {1} determined number is {2}. Converting this to base-3 yields {3}{4}{5}, giving you color {6}",
                 _moduleId, POSITIONS[i], _targetBlobColors[i],
@@ -83,7 +87,7 @@ public class WhiteButtonScript : MonoBehaviour
         Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.BigButtonPress, transform);
         if (!_moduleSolved)
         {
-            _checkTap = false;
+            _longPress = false;
             _buttonHeld = true;
         }
         return false;
@@ -96,7 +100,7 @@ public class WhiteButtonScript : MonoBehaviour
         if (!_moduleSolved)
         {
             _buttonHeld = false;
-            if (!_checkTap)
+            if (!_longPress)
             {
                 AdjustColor((_currentColor + 2) % 3, _currentBlob, _isAdding);
             }
@@ -128,38 +132,34 @@ public class WhiteButtonScript : MonoBehaviour
 
     private void Update()
     {
-        var seconds = (int)BombInfo.GetTime() % 30;
+        var seconds = (int) BombInfo.GetTime() % 30;
         if (seconds != _lastTimerSeconds)
         {
             _lastTimerSeconds = seconds;
-            CycleThings(_lastTimerSeconds);
             if (_buttonHeld)
-                _checkTap = true;
+                _longPress = true;
         }
-    }
-
-    private void CycleThings(int sec)
-    {
         if (!_moduleSolved)
         {
             for (int i = 0; i < 3; i++)
-            {
-                ColorSegments[i].material = i == (sec % 3) ? OnColors[i] : OffColors[i];
-            }
+                ColorSegments[i].material = i == (seconds % 3) ? OnColors[i] : OffColors[i];
+
             if (!_isStriking)
             {
                 for (int i = 0; i < 5; i++)
                 {
-                    LeftLeds[i].material = i == (sec % 5) ? OnLight : OffLight;
-                    RightLeds[i].material = i == (sec % 5) ? OnLight : OffLight;
+                    LeftLeds[i].material = i == (seconds % 5) ? OnLight : OffLight;
+                    RightLeds[i].material = i == (seconds % 5) ? OnLight : OffLight;
                 }
-                _currentColor = sec % 3;
-                _currentBlob = 4 - ((sec + 4) % 5);
-                _isAdding = sec % 2 == 0;
-                if (_colorblindMode)
-                    ColorblindText.text = CBNAMES[_blobColors[_currentBlob]];
+                _currentColor = seconds % 3;
+                _currentBlob = 4 - ((seconds + 4) % 5);
+                _isAdding = seconds % 2 == 0;
+                ColorblindText.text = CBNAMES[_blobColors[_currentBlob]];
             }
         }
+        else
+            ColorblindText.text = "";
+
     }
 
     private void AdjustColor(int channel, int blob, bool add)
@@ -176,14 +176,14 @@ public class WhiteButtonScript : MonoBehaviour
             b = Math.Max(0, Math.Min(2, b + offset));
         _blobColors[blob] = r * 9 + g * 3 + b;
         ColorBlobs[blob].material.color = new Color(
-            (60 + 70 * ((float)r)) / 255,
-            (60 + 70 * ((float)g)) / 255,
-            (60 + 70 * ((float)b)) / 255);
+            (60 + 70 * ((float) r)) / 255,
+            (60 + 70 * ((float) g)) / 255,
+            (60 + 70 * ((float) b)) / 255);
     }
 
     private IEnumerator CheckLogic(bool c)
     {
-        if(_moduleSolved)
+        if (_moduleSolved)
             yield break;
         if (c)
         {
@@ -223,17 +223,102 @@ public class WhiteButtonScript : MonoBehaviour
         }
     }
 #pragma warning disable 0414
-    private readonly string TwitchHelpMessage = "!{0} hold 1 5 [hold on 1, release on 5] | !{0} tap";
+    private readonly string TwitchHelpMessage = "!{0} tap 23 17 5 [tap when the seconds on the timer are these values exactly] | !{0} submit | !{0} colorblind/cb";
 #pragma warning restore 0414
 
     private IEnumerator ProcessTwitchCommand(string command)
     {
-        if (!_moduleSolved)
+        if (Regex.IsMatch(command, @"^\s*(colou?rblind|cb)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            SetColorblindMode(!_colorblindMode);
             yield break;
+        }
+
+        if (Regex.IsMatch(command, @"^\s*(submit|hold)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            yield return null;
+            WhiteButtonSelectable.OnInteract();
+            yield return new WaitForSeconds(1f);
+            WhiteButtonSelectable.OnInteractEnded();
+            yield return new WaitForSeconds(.1f);
+            yield break;
+        }
+
+        var pieces = command.Trim().ToLowerInvariant().Split(new[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+        var skip = pieces[0].EqualsAny("tap", "press", "click", "push") ? 1 : 0;
+        var numbers = new List<int>();
+        foreach (var numPiece in pieces.Skip(skip))
+        {
+            int value;
+            if (!int.TryParse(numPiece, out value) || value < 0 || value >= 60)
+                yield break;
+            numbers.Add(value);
+        }
+        yield return null;
+        yield return "waiting music";
+        while (numbers.Count > 0)
+        {
+            keepWaiting:
+            var ix = numbers.IndexOf((int) BombInfo.GetTime() % 60);
+            if (ix == -1)
+            {
+                yield return "trycancel";
+                goto keepWaiting;
+            }
+            yield return new[] { WhiteButtonSelectable };
+            numbers.RemoveAt(ix);
+        }
     }
 
     public IEnumerator TwitchHandleForcedSolve()
     {
-        yield break;
+        var corrections = new List<int>();
+        for (var blob = 0; blob < 5; blob++)
+        {
+            var curR = _blobColors[blob] / 9;
+            var curG = _blobColors[blob] % 9 / 3;
+            var curB = _blobColors[blob] % 3;
+            var goalR = _targetBlobColors[blob] / 9;
+            var goalG = _targetBlobColors[blob] % 9 / 3;
+            var goalB = _targetBlobColors[blob] % 3;
+
+            if (curR > goalR)
+                corrections.AddRange(Enumerable.Repeat(Enumerable.Range(0, 30).First(i => 4 - (i + 4) % 5 == blob && i % 3 == 0 && i % 2 == 1), curR - goalR));
+            else if (curR < goalR)
+                corrections.AddRange(Enumerable.Repeat(Enumerable.Range(0, 30).First(i => 4 - (i + 4) % 5 == blob && i % 3 == 0 && i % 2 == 0), goalR - curR));
+
+            if (curG > goalG)
+                corrections.AddRange(Enumerable.Repeat(Enumerable.Range(0, 30).First(i => 4 - (i + 4) % 5 == blob && i % 3 == 2 && i % 2 == 1), curG - goalG));
+            else if (curG < goalG)
+                corrections.AddRange(Enumerable.Repeat(Enumerable.Range(0, 30).First(i => 4 - (i + 4) % 5 == blob && i % 3 == 2 && i % 2 == 0), goalG - curG));
+
+            if (curB > goalB)
+                corrections.AddRange(Enumerable.Repeat(Enumerable.Range(0, 30).First(i => 4 - (i + 4) % 5 == blob && i % 3 == 1 && i % 2 == 1), curB - goalB));
+            else if (curB < goalB)
+                corrections.AddRange(Enumerable.Repeat(Enumerable.Range(0, 30).First(i => 4 - (i + 4) % 5 == blob && i % 3 == 1 && i % 2 == 0), goalB - curB));
+        }
+
+        while (corrections.Count > 0)
+        {
+            keepWaiting:
+            var ix = corrections.IndexOf((int) BombInfo.GetTime() % 30);
+            if (ix == -1)
+            {
+                yield return true;
+                goto keepWaiting;
+            }
+            WhiteButtonSelectable.OnInteract();
+            WhiteButtonSelectable.OnInteractEnded();
+            yield return new WaitForSeconds(.1f);
+            corrections.RemoveAt(ix);
+        }
+
+        WhiteButtonSelectable.OnInteract();
+        yield return new WaitForSeconds(1f);
+        WhiteButtonSelectable.OnInteractEnded();
+
+        while (!_moduleSolved)
+            yield return true;
     }
 }
